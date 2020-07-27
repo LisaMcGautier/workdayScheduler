d591ab79b4ac2622cb3ce498519fe05890d3ab98
Lisa McDonald <nixxxxer@aol.com>
2020-07-27 00:55:12 -0400

    Page Layout

---
+++ script.js
@@ -0,0 +1,23 @@
+$(".container").show();
+
+var timeblock = [
+    "8 AM",
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
+];
+
+var block = $("<div>");
+
+
+
+
+
+
+
